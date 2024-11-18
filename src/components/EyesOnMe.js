0026b90ea65f9b3adc90a99bf0c08a3bcea8597d
+ <+>
2024-11-18 15:13:18 +0300

    Event Handling in React

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
-// Code EyesOnMe Component Here
+import React from 'react'
+
+function EyesOnMe() {
+    function handleFocus(event) {
+        event.preventDefault()
+        console.log("Good!")
+    }
+    function handleBlur(event) {
+        event.preventDefault()
+        console.log("Hey! Eyes on me!")
+    }
+  return (
+    <div>
+        <button onFocus={handleFocus} onBlur={handleBlur}>"Eyes on me"</button>
+    </div>
+  )
+}
+
+export default EyesOnMe
